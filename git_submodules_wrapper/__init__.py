444b469a97153edf9f3b9bc5cb98c6df66d97934
Kevin Roy <kiniou@gmail.com>
2019-07-21 19:59:43 +0200

    [setup] simplify `git-sm` console script
    - moved main entrypoint def in __init__.py
    - reuse it in __main__.py
    - changed setup.cfg accordingly

--- git_submodules_wrapper/__init__.py
+++ git_submodules_wrapper/__init__.py
@@ -1,8 +1,12 @@
 """ git-submodules-wrapper - extends 'git submodule' command """
 # pylint: disable=unused-import
-from .cli import cli
 from . import (
-    upstream,
+    cli,
 )
 __appname__ = "git-sm"
 __version__ = "0.0.0"
+
+
+def main():
+    """main module entrypoint"""
+    return cli.cli.main(prog_name=__appname__)
